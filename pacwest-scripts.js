9b4142068966dc6c6c33a2223b6ac4284acd9a81
Valencia <davidalencia@gmail.com>
2022-01-10 11:28:41 -0600

    first commit

---
+++ pacwest-scripts.js
@@ -0,0 +1,15 @@
+<script src="https://kit.fontawesome.com/9a0fb66ac4.js" crossorigin="anonymous"></script>
+<!-- GA Code for SEMRush property start -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-215070398-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-215070398-1');
+</script>
+<!-- GA Code for SEMRush property end -->
+<!-- Klaviyo snippet start -->
+<script async type="text/javascript" src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=NmT8Bp"></script>
+<!-- Klaviyo snippet end -->
